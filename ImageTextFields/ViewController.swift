320675317dbf6a95928ef0971855af1934e02647
Anil <anilyadavjnt@gmail.com>
2024-09-13 13:57:35 +0530

    Initial Commit

---
+++ ImageTextFields/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageTextFields
+//
+//  Created by Anil Yadav on 11/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userName: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
